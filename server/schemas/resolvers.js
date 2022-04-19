398c30f7760806958627af53b1297d4dd34f0806
Peter Riggs <peter92riggs@gmail.com>
2022-04-19 11:30:03 -0600

    resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,80 @@ const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findOne({ _id: context.user._id });
+        return user;
+      }
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No adventurer found with that email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Fix Thee credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveCart: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedCart: args.input } },
+          { new: true, runValidators: true},
+        );
+        return updatedUser;
+      }
+    },
+    removeCart: async (parent, args, context) => {
+      if (context.user) {
+        const cartRemove = await User.findOneAndUpdate(
+          {_id:context.user._id},
+          {$pull: {savedCarts: {cartId: args.cartId}}},
+          {new: true}
+        );
+        return cartRemove;
+      }
+    },
+    addRunes: async (parent, {runes}, context) => {
+      if (context.user) {
+        const increment = Math.abs(runes) * + 1000
+        const newRunes = await User.findOneAndUpdate(
+          {_id:context.user._id},
+          {$inc: {runes: increment}},
+          {new: true},
+        );
+        return newRunes;
+      }
+    },
+    removeRunes: async (parent, {runes, price}, context) => {
+      if (context.user) {
+        const cost = runes - price
+        const newRunes = await User.findOneAndUpdate(
+          {_id:context.user._id},
+          {$set: {runes: cost}},
+          {new: true},
+        );
+        return newRunes;
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
