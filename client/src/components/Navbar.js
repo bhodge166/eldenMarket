bb1b966e4f55b26c4e11b662d06401d8c230009b
Jake Eissler <jakeeissler1@gmail.com>
2022-04-19 14:29:22 -0600

    navbar started and components added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Link from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+      <ul className="navlist">
+        <li>Weapons</li>
+        <li>Armor</li>
+        <li>Creatures</li>
+        <li>Spells</li>
+        <li>Something else but i forgot</li>
+      </ul>
+    </>
+  );
+};
+
+export default Navbar;
