aefbf01ae4532e64fd90146e191cd1eb9185e7cf
Jake Eissler <jakeeissler1@gmail.com>
2022-04-19 14:46:20 -0600

    basic navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,31 @@
 import React from "react";
-// import Link from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav } from "react-bootstrap";
+// import { Navbar } from "react-bootstrap";
+import { Routes } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <ul className="navlist">
-        <li>Weapons</li>
-        <li>Armor</li>
-        <li>Creatures</li>
-        <li>Spells</li>
-        <li>Something else but i forgot</li>
-      </ul>
+      <Nav activeKey={window.location.pathname}>
+        <LinkContainer to="/jakemerch">
+          <Nav.Link>jakemerch</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/kylemerch">
+          <Nav.Link>kylemerch</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/petermerch">
+          <Nav.Link>petermerch</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/bradmerch">
+          <Nav.Link>bradmerch</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/gavinmerch">
+          <Nav.Link>gavinmerch</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/runefarm">
+          <Nav.Link>runefarm</Nav.Link>
+        </LinkContainer>
+      </Nav>
     </>
   );
 };
