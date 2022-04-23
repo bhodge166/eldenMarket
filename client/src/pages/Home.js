66658e9ec3f91c9b8a6c79972fe4f6fa43bc7922
Kyle Henriksen <khenriksenbootcamp@gmail.com>
2022-04-23 12:48:09 -0700

    updating mobile friendly homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -39,112 +39,113 @@ const Home = () => {
 
 
         //  {/* BACKGROUND BEGINS HERE */ }
-        < div className="bkg" > <div className="App App-custom ">
-            <a href="/"><img src={eldenRing} className="intro" alt="intro" /></a>
-            <div>
-                <Navbar />
+        < div className=" App bkg bg-image" >
+            
+                <a href="/"><img src={eldenRing} className="intro bkg" alt="intro" /></a>
+                <div>
+                    <Navbar />
 
-            </div>
-        </div>
-            {/* ---------------------- */}
 
-            {/* LINE BORDER BETWEEN NAVBAR AND HOMEPAGE */}
-            <div className="mb-5" style={{ width: "80%", marginLeft: "150px", border: "3px solid gray", borderRadius: "50px" }} >
-            </div>
-            {/* ----------------------------------------*/}
+                    {/* ---------------------- */}
 
-            <div className="row ">
-
-                {/* JAKES PROFILE */}
-                <div className=" column ms-5 mx-5" >
-                    <LinkContainer to="/jakeMerch">
-                        <h1 className=" navglow text-center font-weight-bold mt-5">Come see Jake the Warrior</h1>
-                    </LinkContainer>
-                    <img src={store} className="merchStyle" alt="" />
-                    <div className="position-absolute mx-5">
-                        <img src={jake} alt="" /></div>
-                    <Card className="bg-dark">
-                        <Card.Body className="brd">
-                            <h1 className="star"><span className="font-weight-bold underline">Name</span> : Jake The Warrior</h1>
-                            <h1 className="star "><span className="font-weight-bold">Goods</span>: Weapons <img src={weapon} className="icon" alt="items" /></h1>
-                            <h1 className="star"><span className="font-weight-bold">Class</span> : Warrior</h1>
-                        </Card.Body>
-                    </Card>
-                </div>
-                {/* ------------- */}
-                {/* KYLES PROFILE */}
-                <div className="column ms-5 mx-5" style={{ position: "relative" }}>
-                    <LinkContainer to="/kyleMerch">
-                        <h1 className="navglow text-center font-weight-bold mt-5" >Visit Kyle The Knight's Store</h1>
-                    </LinkContainer>
-                    <img src={store} className="merchStyle" alt="" />
-                    <div className="position-absolute mx-5 my-5">
-                        <img src={kyle} className="merchStyle" alt="" /></div>
-                    <Card className="bg-dark">
-                        <Card.Body className="brd">
-                            <h1 className="star"><span className="font-weight-bold underline">Name</span> : Kyle</h1>
-                            <h1 className="star "><span className="font-weight-bold">Goods</span>: Armor <img src={armor} className="icon" alt="items" /></h1>
-                            <h1 className="star"><span className="font-weight-bold">Class</span> : Knight</h1>
-                        </Card.Body>
-                    </Card>
-                </div>
-                {/* ------------- */}
-                {/* Peters PROFILE */}
-                <div className="column ms-5 mx-5" style={{ position: "relative" }}>
-                    <LinkContainer to="/peterMerch">
-                        <h1 className=" navglow text-center font-weight-bold mt-2"> Checkout Peters Collectable Pets! </h1>
-                    </LinkContainer>
-                    <img src={store} className="merchStyle" alt="" />
-                    <div style={{ position: "absolute" }}>
-                        <img src={peter} className="peterStyle" alt="petersMerch" /></div>
-                    <Card className="bg-dark">
-                        <Card.Body className="brd">
-                            <h1 className="star"><span className="font-weight-bold underline">Name</span> : Peter</h1>
-                            <h1 className="star "><span className="font-weight-bold">Goods</span>: Pets <img src={pets} className="icon" alt="items" /></h1>
-                            <h1 className="star"><span className="font-weight-bold">Class</span> : Pet Collector</h1>
-                        </Card.Body>
-                    </Card>
-                </div>
-                {/* ------------- */}
-                {/* BRADS PROFILE */}
-
-                <div className="column ms-5 mx-5" style={{ position: "relative" }}>
-                    <LinkContainer to="/bradmerch">
-                        <h1 className="cursor navglow text-center font-weight-bold mt-5">Talismans from Brad the Astrologist!</h1>
-                    </LinkContainer>
-                    <img src={store} className="merchStyle" alt="" />
-                    <div style={{ position: "absolute" }}>
-                        <img src={brad} className="merchStyle" alt="bradMerch" />
+                    {/* LINE BORDER BETWEEN NAVBAR AND HOMEPAGE */}
+                    <div className="mb-5" style={{ width: "80%", marginLeft: "150px", border: "3px solid gray", borderRadius: "50px" }} >
                     </div>
-                    <Card className="bg-dark">
-                        <Card.Body className="brd">
-                            <h1 className="star"><span className="font-weight-bold underline">Name</span> : Brad</h1>
-                            <h1 className="star "><span className="font-weight-bold">Goods</span>: Talismans <img src={items} className="icon" alt="items" /></h1>
-                            <h1 className="star"><span className="font-weight-bold">Class</span> : Astrologist</h1>
-                        </Card.Body>
-                    </Card>
-                </div>
-                {/* ------------- */}
-                {/* GAVINS PROFILE */}
-                <div className="column" style={{ position: "relative" }}>
-                    <LinkContainer to="/gavinMerch">
-                        <h1 className=" navglow text-center font-weight-bold mt-5">Stop by Gavin the Wizard for spells!</h1>
-                    </LinkContainer>
-                    <img src={store} className="merchStyle" alt="" />
-                    <div style={{ position: "absolute" }}>
-                        <img src={gavin} alt="gavinMerch" className="gavinMerch" /></div>
-                    <Card className="bg-dark">
-                        <Card.Body className="brd">
-                            <h1 className="star"><span className="font-weight-bold underline">Name</span> : Gavin</h1>
-                            <h1 className="star "><span className="font-weight-bold">Goods</span>: Spells <img src={spells} className="icon" alt="items" />
-                            </h1><h1 className="star"><span className="font-weight-bold">Class</span> : Wizard</h1>
-                        </Card.Body>
-                    </Card>
-                </div>
-                {/* ------------- */}
+                    {/* ----------------------------------------*/}
 
+                    <div className="row ">
+
+                        {/* JAKES PROFILE */}
+                        <div className=" column ms-5 mx-5" >
+                            <LinkContainer to="/jakeMerch">
+                                <h1 className=" navglow text-center font-weight-bold mt-5">Come see Jake the Warrior</h1>
+                            </LinkContainer>
+                            <img src={store} className="merchStyle" alt="" />
+                            <div className="position-absolute mx-5">
+                                <img src={jake} alt="" /></div>
+                            <Card className="bg-dark">
+                                <Card.Body className="brd">
+                                    <h1 className="star"><span className="font-weight-bold underline">Name</span> : Jake The Warrior</h1>
+                                    <h1 className="star "><span className="font-weight-bold">Goods</span>: Weapons <img src={weapon} className="icon" alt="items" /></h1>
+                                    <h1 className="star"><span className="font-weight-bold">Class</span> : Warrior</h1>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        {/* ------------- */}
+                        {/* KYLES PROFILE */}
+                        <div className="column ms-5 mx-5" style={{ position: "relative" }}>
+                            <LinkContainer to="/kyleMerch">
+                                <h1 className="navglow text-center font-weight-bold mt-5" >Visit Kyle The Knight's Store</h1>
+                            </LinkContainer>
+                            <img src={store} className="merchStyle" alt="" />
+                            <div className="position-absolute mx-5 my-5">
+                                <img src={kyle} className="merchStyle" alt="" /></div>
+                            <Card className="bg-dark">
+                                <Card.Body className="brd">
+                                    <h1 className="star"><span className="font-weight-bold underline">Name</span> : Kyle</h1>
+                                    <h1 className="star "><span className="font-weight-bold">Goods</span>: Armor <img src={armor} className="icon" alt="items" /></h1>
+                                    <h1 className="star"><span className="font-weight-bold">Class</span> : Knight</h1>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        {/* ------------- */}
+                        {/* Peters PROFILE */}
+                        <div className="column ms-5 mx-5" style={{ position: "relative" }}>
+                            <LinkContainer to="/peterMerch">
+                                <h1 className=" navglow text-center font-weight-bold mt-2"> Checkout Peters Collectable Pets! </h1>
+                            </LinkContainer>
+                            <img src={store} className="merchStyle" alt="" />
+                            <div style={{ position: "absolute" }}>
+                                <img src={peter} className="peterStyle" alt="petersMerch" /></div>
+                            <Card className="bg-dark">
+                                <Card.Body className="brd">
+                                    <h1 className="star"><span className="font-weight-bold underline">Name</span> : Peter</h1>
+                                    <h1 className="star "><span className="font-weight-bold">Goods</span>: Pets <img src={pets} className="icon" alt="items" /></h1>
+                                    <h1 className="star"><span className="font-weight-bold">Class</span> : Pet Collector</h1>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        {/* ------------- */}
+                        {/* BRADS PROFILE */}
+
+                        <div className="column ms-5 mx-5" style={{ position: "relative" }}>
+                            <LinkContainer to="/bradmerch">
+                                <h1 className="cursor navglow text-center font-weight-bold mt-5">Talismans from Brad the Astrologist!</h1>
+                            </LinkContainer>
+                            <img src={store} className="merchStyle" alt="" />
+                            <div style={{ position: "absolute" }}>
+                                <img src={brad} className="merchStyle" alt="bradMerch" />
+                            </div>
+                            <Card className="bg-dark">
+                                <Card.Body className="brd">
+                                    <h1 className="star"><span className="font-weight-bold underline">Name</span> : Brad</h1>
+                                    <h1 className="star "><span className="font-weight-bold">Goods</span>: Talismans <img src={items} className="icon" alt="items" /></h1>
+                                    <h1 className="star"><span className="font-weight-bold">Class</span> : Astrologist</h1>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        {/* ------------- */}
+                        {/* GAVINS PROFILE */}
+                        <div className="column" style={{ position: "relative" }}>
+                            <LinkContainer to="/gavinMerch">
+                                <h1 className=" navglow text-center font-weight-bold mt-5">Stop by Gavin the Wizard for spells!</h1>
+                            </LinkContainer>
+                            <img src={store} className="merchStyle" alt="" />
+                            <div style={{ position: "absolute" }}>
+                                <img src={gavin} alt="gavinMerch" className="gavinMerch" /></div>
+                            <Card className="bg-dark">
+                                <Card.Body className="brd">
+                                    <h1 className="star"><span className="font-weight-bold underline">Name</span> : Gavin</h1>
+                                    <h1 className="star "><span className="font-weight-bold">Goods</span>: Spells <img src={spells} className="icon" alt="items" />
+                                    </h1><h1 className="star"><span className="font-weight-bold">Class</span> : Wizard</h1>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        {/* ------------- */}
+                    </div>
+                </div>
             </div>
-        </div >
+       
 
 
     );
