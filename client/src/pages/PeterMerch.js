75d6392c9130309999fa46522f3cc8ca8e49dd06
Gavin Wentzel <gavinwentzel@gmail.com>
2023-03-14 13:59:26 -0400

    changes

--- client/src/pages/PeterMerch.js
+++ client/src/pages/PeterMerch.js
@@ -9,9 +9,10 @@ import {
   CardColumns,
 } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
+import { saveItemsIds, getItemIds } from "../utils/localStorage";
 import { useMutation, useQuery } from "@apollo/client";
-import { QUERY_ALL_PRODUCTS, QUERY_PRODUCT } from "../utils/queries";
-import { ADD_TO_CART } from "../utils/mutations";
+import { QUERY_ALL_PRODUCTS } from "../utils/queries";
+// import { ADD_TO_CART } from "../utils/mutations";
 import Auth from "../utils/auth";
 import peter from "../assets/images/ER_Class_Vagabond.png";
 //navbar stuff
@@ -21,8 +22,14 @@ import eldenRing from "../assets/images/eldenring_new.png";
 import "../css/PeterMerch.css";
 import { LinkContainer } from "react-router-bootstrap";
 const PeterMerch = () => {
-  const [purchaseItem, { error, data2 }] = useMutation(ADD_TO_CART);
+  const [searchedItems, setSearchedItems] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [savedItemIds, setSavedItemIds] = useState(getItemIds());
+  // const [saveItem, { error, data }] = useMutation(ADD_TO_CART);
 
+  useEffect(() => {
+    return () => saveItemsIds(savedItemIds);
+  });
   const currentCategory = "Creatures";
 
   const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
@@ -33,123 +40,113 @@ const PeterMerch = () => {
       (product) => product.category.name === currentCategory
     );
   }
+  console.log(filterProducts());
 
-  const handlePurchaseItem = async (id) => {
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // const handleSaveItem = async (id) => {
+  //   const itemToSave = searchedItems.find((item) => item.id === id);
 
-    if (!token) {
-      return false;
-    }
+  //   // get token
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    try {
-      const { data2 } = await purchaseItem({
-        variables: { products: id },
-      });
-      console.log(data2);
-    } catch (err) {
-      console.error(JSON.stringify(err));
-    }
-  };
+  //   if (!token) {
+  //     return false;
+  //   }
+
+  //   try {
+  //     const { data } = await saveItem({
+  //       variables: { cart: { ...itemToSave } },
+  //     });
+  //     setSavedItemIds([...savedItemIds, itemToSave.id]);
+  //     console.log(data);
+  //   } catch (err) {
+  //     console.error(JSON.stringify(err));
+  //   }
+  // };
 
   return (
     <>
       <div className="App App-custom bkg ">
-        <img
-          src={eldenRing}
-          className="intro"
-          alt="intro"
-          style={{ width: "900px", marginBottom: "50px" }}
-        />
+        <img src={eldenRing} className="intro" alt="intro" />
         <div>
+          <img src={logo} className="App-logo" alt="logo" />
           <Navbar />
-          <div
-            className="mb-5"
-            style={{
-              width: "80%",
-              marginLeft: "150px",
-              border: "3px solid gray",
-              borderRadius: "50px",
-            }}
-          ></div>
         </div>
-
-        <div style={{ width: "100%" }}>
-          <div
-            className="mainContentPeter"
-            style={{
-              position: "relative",
-              width: "100%",
-              height: "1000px",
-              marginTop: "10px",
-            }}
-          >
-            <div className="PeterPet float-left">
-              <img
-                src={peter}
-                alt="petersMerch"
-                height="600px"
-                width="350px"
-                style={{
-                  position: "absolute",
-                  marginTop: 150,
-                  marginLeft: 150,
-                }}
-              />
-            </div>
-
-            <Container
-              className="cardContainer"
-              style={{
-                position: "absolute",
-                top: 100,
-                right: 0,
-                width: "100%",
-              }}
-            >
-              <h2 className="wood-text">
-                {filterProducts().length
-                  ? `Peter's ${filterProducts().length} most prized creatures:`
-                  : "Something went wrong"}
-              </h2>
-              <div className="searchCard row">
-                {filterProducts().map((item) => {
-                  return (
-                    <Card className="resultCard col-sm-4" key={item._id}>
-                      {item.image ? (
-                        <Card.Img
-                          src={item.image}
-                          className="cardImg w-100"
-                          alt={`The cover for ${item.name}`}
-                          variant="top"
-                        />
-                      ) : null}
-                      <Card.Body className="cardBody">
-                        <Card.Title className="titleText">
-                          {item.name}
-                        </Card.Title>
-                        <p className="small descText">{item.description}</p>
-                        <p className="small priceText">
-                          Price: {item.price} Runes
-                        </p>
-
-                        <Card.Text>{item.drops}</Card.Text>
-                        {Auth.loggedIn() && (
-                          <Button
-                            className="btn-block btn-info"
-                            onClick={() => handlePurchaseItem(item._id)}
-                          >
-                            {" "}
-                            Purchase
-                          </Button>
-                        )}
-                      </Card.Body>
-                    </Card>
-                  );
-                })}
-              </div>
-            </Container>
+      </div>
+      <div style={{ width: "100%" }}>
+        <div
+          className="mainContentPeter"
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "1000px",
+            marginTop: "10px",
+          }}
+        >
+          {/* <img
+          src={gavinBg}
+          className="merchantBg bg-image"
+          alt="Merchant Image"
+        /> */}
+          <div className="PeterPet float-left">
+            <img
+              src={peter}
+              alt="petersMerch"
+              height="600px"
+              width="350px"
+              style={{ position: "absolute", marginTop: 150, marginLeft: 150 }}
+            />
           </div>
+
+          <Container
+            className="cardContainer"
+            style={{ position: "absolute", top: 100, right: 0, width: "100%" }}
+          >
+            <h2 className="wood-text">
+              {searchedItems.length
+                ? `Peter's ${searchedItems.length} most prized creatures:`
+                : "Something went wrong"}
+            </h2>
+            <div className="searchCard">
+              {filterProducts().map((item) => {
+                return (
+                  <Card className="resultCard" key={item._id}>
+                    {item.image ? (
+                      <Card.Img
+                        src={item.image}
+                        className="cardImg"
+                        alt={`The cover for ${item.name}`}
+                        variant="top"
+                      />
+                    ) : null}
+                    <Card.Body className="cardBody">
+                      <Card.Title className="titleText">{item.name}</Card.Title>
+                      <p className="small descText">{item.description}</p>
+                      <p className="small priceText">
+                        Price: {item.price} Runes
+                      </p>
+
+                      <Card.Text>{item.drops}</Card.Text>
+                      {/* {Auth.loggedIn() && (
+                      <Button
+                        disabled={savedItemIds?.some(
+                          (savedItemId) => savedItemId === item.id
+                        )}
+                        className="btn-block btn-info"
+                        onClick={() => handleSaveItem(item.id)}
+                      >
+                        {savedItemIds?.some(
+                          (savedItemId) => savedItemId === item.id
+                        )
+                          ? "This creature has been saved!"
+                          : "Save this creature!"}
+                      </Button>
+                    )} */}
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </div>
+          </Container>
         </div>
       </div>
     </>
